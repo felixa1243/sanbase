954c104e00845dce3870b87e7548e9080546763b
felixa1243 <felixarajiph@gmail.com>
2022-04-15 16:25:15 +0700

    improve ui and ux

--- login/script.js
+++ login/script.js
@@ -1,16 +1,15 @@
 const intro=document.getElementById('intro')
 h3=document.querySelector('.row .column h3')
-setTimeout(()=>{
- h3.innerHTML="Get started!"
- h3.style.textDecoration="underline"
-},300)
+h3.innerHTML="Login"
+h3.classList.add('button')
+h3.addEventListener('click',()=>{
+  h3.classList.add('button-click')
+})
 const nonmember=document.querySelector('.row:nth-child(2)')
 const form=document.querySelector('.d-none')
 form.style.transitionDelay='1s'
 intro.addEventListener("click",()=>{
   intro.classList.add('t-rotate')
-  intro.style.transition='1s'
-  intro.style.maxWidth='50%'
   intro.parentNode.removeChild(h3)
   setTimeout(function() {
   form.classList.remove('d-none')
@@ -31,3 +30,21 @@ h3.addEventListener("click",()=>{
   nonmember.classList.remove('d-none')
   }, 1500);
 } )
+form.querySelector('button').onclick=()=>{
+  form.querySelector('button').style.background="#929292"
+}
+/*
+$("#intro").click(function(){
+$("form").animate({
+  oppacity:"show"
+},{
+  easing:"easein"
+})
+})
+$(".row:nth-child(1) .column h3").html("Login")
+$(".row:nth-child(1) .column h3").click(function(){
+  $("form").animate({
+    oppacity:"show"
+  },{easing:"easein"},900)
+  $(".row:nth-child(1) .column h3").hide()
+})*/
